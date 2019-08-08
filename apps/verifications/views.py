8686a8870d0fef465c4d4fe4c33df2c951559447
CoderGwt <1341680843@qq.com>
2019-08-08 21:50:27 +0800

    验证相关app

---
+++ apps/verifications/views.py
@@ -0,0 +1,15 @@
+from django.views import View
+from django.shortcuts import render
+
+# Create your views here.
+
+
+class ImageCode(View):
+    """
+    define image verification view
+    /image_code/<uuid:image_code_id>/
+    """
+    '''
+    '''
+    def get(self, request):
+        pass
