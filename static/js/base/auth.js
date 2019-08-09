e65622db63c635f875ec710866f09acb60216e4b
CoderGwt <1341680843@qq.com>
2019-08-09 22:59:13 +0800

    验证用户名是否存在 js

--- static/js/base/auth.js
+++ static/js/base/auth.js
@@ -1,6 +1,51 @@
 $(function () {
   let $img = $(".form-item .captcha-graph-img img");
   let sImageCodeId = "";  // 定义一个保存图像验证码的id
+  let $username = $("#username");
+
+  $username.blur(function () {
+    check_username();
+  });
+  function check_username() {
+    let username = $username.val();
+    if (username === "") {
+      // alert("用户名不能为空！");
+      message.showError('用户名不能为空！');
+      return
+    }
+    if (!(/^\w{5,20}$/).test(username)) {
+      // alert("请输入5-20个字符的用户名");
+      message.showError('请输入5-20个字符的用户名');
+      return
+    }
+
+    $.ajax({
+      url: "/verifications/username/" + username + "/",
+      type: 'GET' ,
+      dataType: "json",
+      success: function (res) {
+        console.log(res);
+        if(res.data.count === 0){
+          message.showInfo(res.data.msg);
+        }else{
+          message.showError(res.data.msg)
+        }
+        // alert(res.data.msg);  // 后端通过to_json_data封装返回信息
+        // alert(res.msg);  // 后端直接返回的是JsonResponse
+        // if(res.count == 0){
+        //   alert("可用")
+        // }else{
+        //   alert("不可可用")
+        // }
+      },
+      fail: function(error){
+        // alert(error);
+        message.showError("服务器错误，请稍后充实")
+      }
+    })
+
+
+  }
 
 
   generateImageCode();  // 生成图片验证码
@@ -28,5 +73,7 @@ $(function () {
     let imageCodeUrl = "/verifications/image_code/" + sImageCodeId + "/";
     // 3、修改验证码图片src地址
     $img.attr('src', imageCodeUrl)
+
+
   }
 });
