7566bf94846cd1cd8d33ddcdf625d2ddda2f4286
CoderGwt <1341680843@qq.com>
2019-08-09 22:59:28 +0800

    add check username url

--- apps/verifications/urls.py
+++ apps/verifications/urls.py
@@ -1,8 +1,9 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
 app_name = 'verification'
 
 urlpatterns = [
-    path("image_code/<uuid:image_code_id>/", views.ImageCode.as_view(), name='image_code')
+    path("image_code/<uuid:image_code_id>/", views.ImageCode.as_view(), name='image_code'),
+    re_path('username/(?P<username>\w{5,20})/', views.CheckUsernameView.as_view(), name='check_username'),
 ]
