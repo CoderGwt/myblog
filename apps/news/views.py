cc777379a4cf95053c560c716ee4d5c8d7ef9fdf
CoderGwt <1341680843@qq.com>
2019-09-11 17:47:57 +0800

    渲染tags

--- apps/news/views.py
+++ apps/news/views.py
@@ -2,12 +2,14 @@ from django.views import View
 from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Tag
 
 
 class IndexView(View):
     def get(self, request):
-        return render(request, 'news/index.html')
+        # todo 使用only进行select查询你，优化查询效率
+        tags = Tag.objects.only('id', 'name').filter(is_delete=False).all()
+        return render(request, 'news/index.html', locals())
 
 
 class SearchView(View):
